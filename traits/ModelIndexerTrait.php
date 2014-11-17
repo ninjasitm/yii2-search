54baa08a1e347535be54afe1f1f610600adf4122
Callcentric Lab1 <mpaul@callcentric.com>
2014-11-17 14:19:49 -0500

    Wrapped search updated in try/catch

--- traits/ModelIndexerTrait.php
+++ traits/ModelIndexerTrait.php
@@ -13,20 +13,26 @@ trait ModelIndexerTrait
 {
 	protected function updateSearchEntry($event)
 	{
-		$module = \Yii::$app->getModule('nitm-search');
-		if(is_object($module))
-		{
-			$indexer = $module->getIndexer();
-			$attributes = $indexer::normalize($event->sender->findOne($event->sender->getId())->toArray());
-			$attributes['_md5'] = $module->fingerprint($attributes);
-			$options = [
-				'url' => $this->isWhat().'/'.$event->sender->getId(), 
-				json_encode($attributes), 
-				true
-			];
-			return $indexer::api('put', $options);
+		try {
+			$module = \Yii::$app->getModule('nitm-search');
+			if(is_object($module))
+			{
+				$indexer = $module->getIndexer();
+				$attributes = $indexer::normalize($event->sender->findOne($event->sender->getId())->toArray());
+				$attributes['_md5'] = $module->fingerprint($attributes);
+				$options = [
+					'url' => $this->isWhat().'/'.$event->sender->getId(), 
+					json_encode($attributes), 
+					true
+				];
+				return $indexer::api('put', $options);
+			}
+			return false;
+		} catch \Exception $e {
+			//Only throw this error if we're debugging
+			if(YII_DEBUG)
+				throw $e;
 		}
-		return false;
 	}
 }
 ?>
